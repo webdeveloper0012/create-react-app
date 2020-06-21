f2d444ee7e11e74170f25442b5529434875fa381
agustinremesal1990 <agustinremesal27@gmail.com>
2020-06-21 16:33:55 +1000

    Updates

---
+++ precache-manifest.9274cb1121e9d1c9200ff951a9df69b2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afa60a1b131cb9bb56fc70a5f7e25983",
+    "url": "/create-react-app/index.html"
+  },
+  {
+    "revision": "516479b3ff5edea2ea94",
+    "url": "/create-react-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "652a599d1a99efccd345",
+    "url": "/create-react-app/static/js/2.a430f49c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/create-react-app/static/js/2.a430f49c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "516479b3ff5edea2ea94",
+    "url": "/create-react-app/static/js/main.be1c585c.chunk.js"
+  },
+  {
+    "revision": "dd60658a4baacad79806",
+    "url": "/create-react-app/static/js/runtime-main.7dfc74c8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/create-react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
